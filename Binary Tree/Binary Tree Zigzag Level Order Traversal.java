49af9da893945a973745bee1fbeec3c258f3249d
Vishwa V <vvishwa2526@gmail.com>
2025-06-30 19:57:03 +0530

    Create Binary Tree Zigzag Level Order Traversal

---
+++ Binary Tree/Binary Tree Zigzag Level Order Traversal.java
@@ -0,0 +1,64 @@
+103. Binary Tree Zigzag Level Order Traversal
+
+Given the root of a binary tree, return the zigzag level order traversal of its nodes' values. (i.e., from left to right, then right to left for the next level and alternate between).
+
+Example 1:
+
+
+Input: root = [3,9,20,null,null,15,7]
+Output: [[3],[20,9],[15,7]]
+Example 2:
+
+Input: root = [1]
+Output: [[1]]
+Example 3:
+
+Input: root = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 2000].
+-100 <= Node.val <= 100
+  --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+Solution
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Deque<TreeNode> d = new LinkedList<>();
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) {
+            return result;
+        }
+
+        d.offer(root);
+        boolean leftToRight = true;
+
+        while (!d.isEmpty()) {
+            int levelSize = d.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < levelSize; i++) {
+                if (leftToRight) {
+                    TreeNode node = d.pollFirst();
+                    level.add(node.val);
+                    if (node.left != null) d.offerLast(node.left);
+                    if (node.right != null) d.offerLast(node.right);
+                } else {
+                    TreeNode node = d.pollLast();
+                    level.add(node.val);
+                    if (node.right != null) d.offerFirst(node.right);
+                    if (node.left != null) d.offerFirst(node.left);
+                }
+            }
+
+            result.add(level);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+}
+
+ 
